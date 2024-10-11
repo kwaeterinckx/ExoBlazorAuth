9c3ac51510e2c4c7dacfa3b8cafca4f4a55dc256
kwaeterinckx <159497772+kwaeterinckx@users.noreply.github.com>
2024-10-11 16:26:25 +0200

    Add Users

---
+++ ExoBlazor-FE/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ExoBlazor_FE.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
---
+++ ExoBlazor-FE/Pages/Users.razor.cs
@@ -0,0 +1,25 @@
+﻿using ExoBlazor_FE.Models;
+using Microsoft.JSInterop;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace ExoBlazor_FE.Pages
+{
+    public partial class Users
+    {
+        public List<UserModel> AllUsers { get; set; } = new List<UserModel>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadData();
+        }
+
+        async Task LoadData()
+        {
+            string token = await JSRuntime.InvokeAsync<string>("localStorage.getItem", "token");
+            HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            AllUsers = await HttpClient.GetFromJsonAsync<List<UserModel>>("User");
+        }
+    }
+}
